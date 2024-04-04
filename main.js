051d0fb068af54fa598aeb46f864368863ae0bec
BadmaArs <134961738+BadmaArs@users.noreply.github.com>
2024-04-04 18:10:49 +0300

    helper

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import gsap from "gsap";
 
 const canvas = document.querySelector("canvas.webgl");
-const helper = new THREE.AxesHelper()
+const helper = new THREE.AxesHelper();
 const modelThree = "./t34.glb";
 // Scene
 const scene = new THREE.Scene();
-scene.add(helper)
+// scene.add(helper)
 // GLTF Loader
 let pokeball = null;
 const gltfLoader = new GLTFLoader();
@@ -53,7 +53,7 @@ const transformPokeball = [
   },
   {
     scale: { x: 0.5, y: 0.5, z: 0.5 },
-    rotationY: 55 * Math.PI / 180,
+    rotationY: (55 * Math.PI) / 180,
     rotationZ: 0,
     positionX: 0.6,
     positionY: 0,
@@ -89,11 +89,12 @@ window.addEventListener("scroll", () => {
         ease: "power2.inOut",
         y: transformPokeball[currentSection].rotationY,
         z: transformPokeball[currentSection].rotationZ,
-        onUpdate: function () { // добавил эту строку
-          if(sizes.width < 600) {
+        onUpdate: function () {
+          // добавил эту строку
+          if (sizes.width < 600) {
             camera.lookAt(pokeball.position); // и эту строку
           }
-        }
+        },
       });
       gsap.to(pokeball.position, {
         duration: 4.5,
@@ -111,18 +112,17 @@ window.addEventListener("scroll", () => {
   }
 });
 
-
 // Sizes
 
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-window.addEventListener('resize', function() {
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
-})
-console.log(sizes.height, sizes.width)
+window.addEventListener("resize", function () {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+});
+console.log(sizes.height, sizes.width);
 // Camera
 const camera = new THREE.PerspectiveCamera(
   35,
@@ -131,11 +131,11 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 if (sizes.width < 600) {
-  camera.position.z = 7
-  camera.position.y = 1
-  camera.position.x = -0.7
-  camera.rotation.x = -0.3
-  camera.rotation.y = 0
+  camera.position.z = 7;
+  camera.position.y = 1;
+  camera.position.x = -0.7;
+  camera.rotation.x = -0.3;
+  camera.rotation.y = 0;
 } else {
   camera.position.z = 5;
   camera.position.y = 0.5;
